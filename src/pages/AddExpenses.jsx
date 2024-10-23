2a426b4e64d561a262d1acab2c77894baaf3e24a
SarahCant <eaa23smtwc@students.eaaa.dk>
2024-10-23 11:26:12 +0200

    Code cleanup

--- src/pages/AddExpenses.jsx
+++ src/pages/AddExpenses.jsx
@@ -1,100 +1,3 @@
-/* import "../css/Sarah.css";
-import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import Category from "../components/Category";
-
-export default function AddExpenses() {
-  const [categories, setCategories] = useState([]);
-  const [selectedCategory, setSelectedCategory] = useState(null);
-  const [cost, setCost] = useState("");
-  const navigate = useNavigate();
-
-  // Fetch categories
-  useEffect(() => {
-    let isMounted = true;
-
-    async function fetchCategories() {
-      const response = await fetch(
-        "https://web-app-c295f-default-rtdb.firebaseio.com/category.json"
-      );
-      if (!isMounted) return;
-      const data = await response.json();
-      const categoryArray = Object.keys(data).map((key) => ({
-        id: key,
-        ...data[key],
-      }));
-      setCategories(categoryArray);
-    }
-
-    fetchCategories();
-
-    return () => {
-      isMounted = false; // cleanup
-    };
-  }, []);
-
-  // handle form submission
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    if (!cost || !selectedCategory) {
-      alert("Hov! Du har vidst ikke udfyldt alle felter");
-      return;
-    }
-
-    // Update the budget in Firebase
-    const categoryToUpdate = categories.find(
-      (cat) => cat.id === selectedCategory
-    );
-    const updatedBudget = categoryToUpdate.budget - parseFloat(cost);
-
-    // Update Firebase with the new budget
-    await fetch(
-      `https://web-app-c295f-default-rtdb.firebaseio.com/category/${selectedCategory}.json`,
-      {
-        method: "PATCH",
-        body: JSON.stringify({ budget: updatedBudget }),
-      }
-    );
-
-    // Optionally, fetch updated categories again
-    // fetchCategories();
-
-    navigate("/"); // navigate to main page
-  };
-
-  return (
-    <div className="ae_main">
-      <p>Udgift</p>
-      <div className="input-group">
-        <div className="budget-input">
-          <input
-            type="number"
-            placeholder="Tilføj beløb"
-            value={cost}
-            onChange={(e) => setCost(e.target.value)}
-            required
-          />
-        </div>
-      </div>
-
-      <p className="ae_txt">Kategori</p>
-      <section className="ae_category">
-        <Category
-          categories={categories}
-          selectedCategory={selectedCategory}
-          onSelectCategory={setSelectedCategory}
-        />
-      </section>
-
-      <button type="submit" className="submit-btn" onClick={handleSubmit}>
-        Gem
-      </button>
-    </div>
-  );
-}
- */
-
 import "../css/Sarah.css";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
@@ -179,7 +82,14 @@ export default function AddExpenses() {
         </div>
       </div>
 
-      <p className="ae_txt">Kategori</p>
+      <p>Note</p>
+      <div className="input-group">
+        <div className="budget-input">
+          <input type="text" placeholder="Tilføj note" />
+        </div>
+      </div>
+
+      <p className="ae_txt">Vælg kategori</p>
       <section className="ae_category">
         <Category
           categories={categories}
@@ -187,6 +97,8 @@ export default function AddExpenses() {
           onSelectCategory={setSelectedCategory}
         />
       </section>
+
+      <button className="ae_addquick_btn">+Opret som quickadd</button>
 
       <button type="submit" className="submit-btn" onClick={handleSubmit}>
         Gem
