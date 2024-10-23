868f1851d33e4dedb4a2ffb059e51fc9c9dd2e9a
Sofie Bluhm Kristensen <fieblomsten@gmail.com>
2024-10-23 13:36:07 +0200

    animation

--- src/components/Animation.jsx
+++ src/components/Animation.jsx
@@ -1,3 +1,37 @@
+import "../css/Sofie.css";
+import { useEffect } from "react";
+
 export default function Animation() {
-  return <div></div>;
+  useEffect(() => {
+    const logoElement = document.querySelector(".logo_animation");
+
+    // Add class to make logo visible after mounting
+    setTimeout(() => {
+      logoElement.classList.add("start-visible");
+    }, 200); // Slight delay to ensure visibility change is registered
+
+    // Additional audio logic can go here
+  }, []);
+
+  return (
+    <div>
+      <h1 className="h1_forside">Forside</h1>
+      <div className="bg_animation">
+        <div className="logo_rel">
+          <img
+            className="logo_animation"
+            src="../public/img/logo_nofire.png"
+            alt="logo"
+          />
+          <img
+            className="streak_animation"
+            src="../public/img/streak.png"
+            alt=""
+          />
+        </div>
+
+        <h1 className="h1_animation">ØkonomILD</h1>
+      </div>
+    </div>
+  );
 }
