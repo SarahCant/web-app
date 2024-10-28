1555fe4340a04469a53dce3d206494122efcede9
Sofie Bluhm Kristensen <fieblomsten@gmail.com>
2024-10-28 11:08:52 +0100

    mybudget updatecategory

--- src/pages/MyBudget.jsx
+++ src/pages/MyBudget.jsx
@@ -2,10 +2,16 @@ import Category from "../components/Category";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import BarChart from "../components/BarChart";
+import { useNavigate } from "react-router-dom";
 
 export default function MyBudget() {
   const [categories, setCategories] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState(null);
+  const navigate = useNavigate();
+
+  const handleCategoryClick = (categoryId) => {
+    navigate(`/updatecategory/${categoryId}`);
+  };
 
   useEffect(() => {
     async function fetchCategories() {
@@ -35,6 +41,7 @@ export default function MyBudget() {
         categories={categories}
         selectedCategory={selectedCategory}
         onSelectCategory={setSelectedCategory}
+        onClickCategory={handleCategoryClick}
       />
       <div className="fp_addcategory_flex">
         <Link to="/addcategory">
