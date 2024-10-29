8170f1f4b45e3abeb475f42b1bb3f23cf3adbaa7
Sofie Bluhm Kristensen <fieblomsten@gmail.com>
2024-10-29 19:16:32 +0100

    alertbox

---
+++ src/components/AlertBox.jsx
@@ -0,0 +1,33 @@
+import "../css/sofie.css";
+
+export default function AlertBox({
+  alertMessage,
+  showConfirmButtons = false,
+  onConfirm,
+  onCancel,
+  onOk,
+}) {
+  return (
+    <div className="alert_box">
+      <img className="logo_alert" src="../public/img/logo_fire.png" alt="" />
+      <span className="circle_alert"></span>
+      <p className="p_alert">{alertMessage}</p>
+      <div className="btn_alert">
+        {showConfirmButtons ? (
+          <>
+            <button className="btn" onClick={onConfirm}>
+              Ja
+            </button>
+            <button className="btn" onClick={onCancel}>
+              Annuller
+            </button>
+          </>
+        ) : (
+          <button className="btn" onClick={onOk}>
+            Ok
+          </button>
+        )}
+      </div>
+    </div>
+  );
+}
